7a7cf5f9bc2843d6236b364c22bcd50c09738c28
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-01 16:22:08 +0600

    docs: udpated application description

--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ssh-config-manager-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "CLI application to manage ssh config file",
+	Long: `A CLI application to manage ssh config file which resides in $HOME/.ssh/config.
+This application aims to add hosts, remove hosts and edit host parameters in the configuration file.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
