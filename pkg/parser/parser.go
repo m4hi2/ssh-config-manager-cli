e322ea212fee7de4e7d92fc9892d90578c0e3072
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-01 20:33:16 +0600

    feat: extracion of configuration pairs
    Extracting configuration pairs and holding them in a config pair struct.

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,9 +1,27 @@
 package parser
 
+import "strings"
+
 type Config struct {
 	Host     string
 	HostName string
 	User     string
 	Port     string
 	IdFile   string
+}
+
+type configPair struct {
+	FieldName string
+	Value     string
+}
+
+func extractConfigPairs(line string) configPair {
+	configLine := strings.Split(line, " ")
+	fieldName := strings.TrimSpace(configLine[0])
+	value := strings.TrimSpace(configLine[1])
+	config := configPair{
+		FieldName: fieldName,
+		Value:     value,
+	}
+	return config
 }
