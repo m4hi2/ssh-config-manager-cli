299003f56fc5c0dda48e07a9c41f69d61a05261c
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-01 20:33:48 +0600

    test: config pair extraction

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"bufio"
 	"os"
 	"testing"
 )
@@ -10,5 +11,54 @@ func TestCanReadConfigFile(t *testing.T) {
 
 	if err != nil {
 		t.Fatal("File can not be read")
+	}
+}
+
+func TestExtractConfigPairs(t *testing.T) {
+	configFile, err := os.Open("testdata/config")
+	hostConfig := configPair{
+		FieldName: "Host",
+		Value:     "hello",
+	}
+
+	hostNameConfig := configPair{
+		FieldName: "HostName",
+		Value:     "10.0.0.1",
+	}
+	userConfig := configPair{
+		FieldName: "User",
+		Value:     "user",
+	}
+	portConfig := configPair{
+		FieldName: "Port",
+		Value:     "22",
+	}
+	if err != nil {
+		t.Fatal("File Can not be read")
+	}
+	s := bufio.NewReader(configFile)
+	value, _, _ := s.ReadLine()
+	config := extractConfigPairs(string(value))
+
+	if config != hostConfig {
+		t.Fatal("Host Config Cannot be read")
+	}
+
+	value, _, _ = s.ReadLine()
+	config = extractConfigPairs(string(value))
+	if config != hostNameConfig {
+		t.Fatal("HostName Config Cannot be read")
+	}
+
+	value, _, _ = s.ReadLine()
+	config = extractConfigPairs(string(value))
+	if config != portConfig {
+		t.Fatal("Port Config Cannot be read")
+	}
+
+	value, _, _ = s.ReadLine()
+	config = extractConfigPairs(string(value))
+	if config != userConfig {
+		t.Fatal("user Config Cannot be read")
 	}
 }
