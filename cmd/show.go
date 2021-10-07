100330f603f9f5fd2ae8843f5d55bbbf7c992d10
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-07 20:18:29 +0600

    feat: initial impl of readdump/show command

--- cmd/show.go
+++ cmd/show.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -32,7 +33,9 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("show called")
+		filepath := os.Getenv("HOME") + "/.ssh/config"
+		fileContent, _ := os.ReadFile(filepath)
+		fmt.Println(string(fileContent))
 
 	},
 }
