37d3de282501e79552e1d0b7992c575055d9eced
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-13 10:18:06 +0600

    test: adding new host to config file

--- pkg/sshconfigmanagerintenal/sshconfigmanagerinternal_test.go
+++ pkg/sshconfigmanagerintenal/sshconfigmanagerinternal_test.go
@@ -1,8 +1,11 @@
 package sshconfigmanagerintenal
 
 import (
+	"log"
 	"os"
 	"testing"
+
+	"github.com/m4hi2/ssh-config-manager-cli/pkg/parser"
 )
 
 func TestFileBackUp(t *testing.T) {
@@ -16,6 +19,34 @@ func TestFileBackUp(t *testing.T) {
 	if string(file) != string(backedUpFile) {
 		t.Fatalf("Original file and backup file are not same")
 	}
+	os.Remove("testdata/config.bak")
+
+}
+
+func TestLowLevelAdd(t *testing.T) {
+	sampleConfig := parser.Config{
+		Host:     "gg",
+		HostName: "hallo",
+		Port:     "22",
+		User:     "m4hi2",
+	}
+	add(sampleConfig, "testdata/config")
+
+	fileSample, _ := os.ReadFile("testdata/config_add_sample")
+	fileAdded, _ := os.ReadFile("testdata/config")
+
+	if string(fileAdded) != string(fileSample) {
+		// clean up
+		backUpFile, _ := os.ReadFile("testdata/config.bak")
+		os.WriteFile("testdata/config", []byte(backUpFile), 0644)
+		os.Remove("testdata/config.bak")
+		log.Fatalln("Config file not created properly.")
+
+	}
+
+	// clean up
+	backUpFile, _ := os.ReadFile("testdata/config.bak")
+	os.WriteFile("testdata/config", []byte(backUpFile), 0644)
 	os.Remove("testdata/config.bak")
 
 }
