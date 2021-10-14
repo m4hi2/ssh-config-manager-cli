833302b9ffaf50a11f1238ff2763f3585a4bb1c9
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-14 12:35:58 +0600

    change: log level for backup func now doesn't exit

--- pkg/sshconfigmanagerintenal/sshconfigmanagerinternal.go
+++ pkg/sshconfigmanagerintenal/sshconfigmanagerinternal.go
@@ -11,7 +11,7 @@ import (
 func backUpFile(filePath string) {
 	originalFile, err := os.ReadFile(filePath)
 	if err != nil {
-		log.Fatalf("Can not find ssh config file\n Error: %s", err)
+		log.Printf("Can not find ssh config file\n Error: %s", err)
 	}
 
 	backUp, _ := os.Create(fmt.Sprintf("%s.bak", filePath))
