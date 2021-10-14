1b52129c1a1c81ff3519d29ff3a0318aceeb24ab
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-14 08:59:57 +0600

    docs: provided cmd description for prompt command

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -24,13 +24,8 @@ import (
 // promptCmd represents the prompt command
 var promptCmd = &cobra.Command{
 	Use:   "prompt",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Provides a prompt to the user for adding host config interactively",
+	Long:  `Provides a prompt to the user for adding host config interactively`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("prompt called")
 	},
@@ -38,14 +33,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	addCmd.AddCommand(promptCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// promptCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// promptCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
